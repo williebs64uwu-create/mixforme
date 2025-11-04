466c1905cfbfa4a1537fe7cf3756e44ea0b7cca3
Willie Inspired <williebs64uwu@gmail.com>
2025-11-03 22:16:26 -0500

    Update and rename App.jsx.txt to App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Dashboard from './components/Dashboard'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-gray-950 via-gray-900 to-indigo-950">
+      <Dashboard />
+    </div>
+  )
+}
+
+export default App
